22e7810285e5fab5ae300eac8bd8d36c06997946
Hazem Abd el-Aziz <hazemsoliman096@gmail.com>
2022-10-15 22:46:52 +0200

    deleted a.sql

--- a.sql
+++
@@ -1,9 +0,0 @@
-SELECT e.employee_id,
-       e.first_name,
-       e.last_name,
-       d.department_name,
-       e.salary AS old_salary,
-       e.salary * (1 + d.p) AS new_salary
-  FROM employees   e inner join 
-       (select department_id,department_name, pctIncrease(department_id) as p  from departments ) d on  e.department_id = d.department_id
- ORDER BY 1;
